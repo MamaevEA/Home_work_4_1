4a27d41533657203bb8c2cf43110b2dd12fc59f8
MamaevEA <mamaevea@gmail.com>
2022-08-10 09:30:06 +0500

    Done Max

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA < numB)
+{
+    Console.Write("Максимальное значение: ");
+    Console.WriteLine(numB);
+}
+
+else
+{
+    Console.Write("Максимальное значение: ");
+    Console.WriteLine(numA);
+}
